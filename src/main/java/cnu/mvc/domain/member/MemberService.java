728de2dc9f46b140fda0b01c09479e79000b9b04
Minjae <ashclothes01@o.cnu.ac.kr>
2025-04-14 17:45:03 +0900

    add_function

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -10,11 +10,23 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member join(Member member){
+        // 들어온 멤버의 email을 가져옵니다.
+        String joinEmail = member.getEmail();
+        //
+        if (findByEmail(joinEmail) != null) {
+            throw new IllegalArgumentException("이미 존재하는 이메일 계정입니다.");
+        }
         return memberRepository.save(member);
     }
 
     public Member validateMember(String email, String pwd) {
-        Member findMember = findById(1L);
+        Member findMember = memberRepository.findByEmail(email);
+        if (findMember == null) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호를 확인해주세요.");
+        }
+        if (!findMember.getPwd().equals(pwd)) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호를 확인해주세요.");
+        }
         return findMember;
     }
 
