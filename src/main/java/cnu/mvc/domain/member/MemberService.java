9a0b3fcaabec67a3fc3611f7ea544ba3511f5318
leekh2002 <lkh4342@naver.com>
2025-04-14 20:05:26 +0900

    회원가입시 중복이메일 체크 & 로그인시 검증기능 추가

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -9,13 +9,20 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public Member join(Member member){
+    public Member join(Member member) {
+        Member byEmail = findByEmail(member.getEmail());
+        if (byEmail != null) {
+            throw new RuntimeException("이미 존재하는 이메일 계정입니다.");
+        }
         return memberRepository.save(member);
     }
 
     public Member validateMember(String email, String pwd) {
-        Member findMember = findById(1L);
-        return findMember;
+        Member findMember = findByEmail(email);
+        if (findMember != null && findMember.getPwd().equals(pwd)) {
+            return findMember;
+        }
+        throw new RuntimeException("이메일 또는 비밀번호를 확인해주세요.");
     }
 
     public Member findById(Long id) {
