dabf4c0d0b0a2ce724a8f1ba17ab814aa6311171
song <162600706+song6-1@users.noreply.github.com>
2025-04-16 16:28:49 +0900

    Update MemberService.java

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -24,7 +24,12 @@ public class MemberService {
 
     // 구현
     public Member findByEmail(String email) {
-        return memberRepository.findByEmail(email);
+        for (var member : store.values()) {
+            if (member.getEmail().equals(email)) {
+                return member;
+            }
+        }
+        return null;
     }
 
 }
