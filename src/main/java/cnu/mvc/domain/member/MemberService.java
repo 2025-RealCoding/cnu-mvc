11d5c4a6ff40f851af63a3efcc8ea18c90e0fa80
bbrrii <rlatkdcjf1244@gmail.com>
2025-04-18 10:24:03 +0900

    수정

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -10,11 +10,17 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member join(Member member){
+
+        if (memberRepository.findByEmail(member.getEmail()) != null) throw new IllegalStateException("“이미 존재하는 이메일 계정입니다.");
         return memberRepository.save(member);
     }
 
     public Member validateMember(String email, String pwd) {
-        Member findMember = findById(1L);
+        Member findMember = findByEmail(email);
+
+        if (findMember == null || !findMember.getPwd().equals(pwd)) {
+            throw new IllegalStateException("이메일 또는 비밀번호를 확인해주세요. ");
+        }
         return findMember;
     }
 
