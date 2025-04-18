382485c4b67a4668af71595716daec7f22a31a03
2ndSilverMan <2ndsilverman@gmail.com>
2025-04-18 12:59:42 +0900

    202002520 EunMinSoo answer

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -9,12 +9,19 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public Member join(Member member){
+    public Member join(Member member) {
+        if (findByEmail(member.getEmail()) != null) {
+            throw new RuntimeException("이미 존재하는 이메일 계정입니다.");
+        }
         return memberRepository.save(member);
     }
 
     public Member validateMember(String email, String pwd) {
-        Member findMember = findById(1L);
+
+        Member findMember = findByEmail(email);   // 이메일을 기반한 아이디를 통해 멤버 식별
+        if (findMember == null || !findMember.getPwd().equals(pwd)) {
+            throw new RuntimeException("이메일 또는 비밀번호를 확인해주세요");  // 아이디 또는 비밀번호가 잘못되었을 경우 { throw }
+        }
         return findMember;
     }
 
