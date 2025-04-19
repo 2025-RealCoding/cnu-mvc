4bbf02e578cf7b7b6c8c85a5ec1fd33955dc9ee4
정재현 <mhgrid@naver.com>
2025-04-19 17:17:44 +0900

    202302616-정재현 최종 제출

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -9,22 +9,29 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public Member join(Member member){
+    // 1-2) 회원가입 시 중복 이메일 검증
+    public Member join(Member member) {
+        if (memberRepository.findByEmail(member.getEmail()) != null) {
+            throw new IllegalStateException("이미 존재하는 이메일 계정입니다.");
+        }
         return memberRepository.save(member);
     }
 
+    // 2-1) 로그인 검증
     public Member validateMember(String email, String pwd) {
-        Member findMember = findById(1L);
+        Member findMember = memberRepository.findByEmail(email);
+        if (findMember == null || !findMember.getPassword().equals(pwd)) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호를 확인해주세요.");
+        }
         return findMember;
     }
 
+    // 기타 기능
     public Member findById(Long id) {
         return memberRepository.findById(id);
     }
 
-    // 구현
     public Member findByEmail(String email) {
         return memberRepository.findByEmail(email);
     }
-
 }
