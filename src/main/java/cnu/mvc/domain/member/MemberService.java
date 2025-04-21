77ff24956992b2ccd8b7917570a49bdedc87e675
mingsiyo <minsik0512@naver.com>
2025-04-21 16:07:35 +0900

    Update MemberService.java

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -18,7 +18,7 @@ public class MemberService {
 
     public Member validateMember(String email, String pwd) {
         Member findMember = findByEmail(email);
-        if (!findMember.getPwd().equals(pwd)) {
+        if (findMember == null || !findMember.getPwd().equals(pwd)) {
             throw new RuntimeException("이메일 또는 비밀번호를 확인해주세요.");
         }
         return findMember;
